06c31dd5592c15c8db1fc9071951c4ac7a02d4ba
chenchen <chenchen_9694@outlook.com>
2020-02-01 16:43:14 +0800

    test adaption

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
  * @Author: OBKoro1
  * @Date: 2020-01-13 15:29:02
  * @LastEditors: chenchen
- * @LastEditTime: 2020-02-01 16:20:44
+ * @LastEditTime: 2020-02-01 16:40:40
  -->
 <template>
   <div class="hello">
@@ -30,10 +30,14 @@ export default {
 
 <style lang="less" scoped>
 .hello {
+  font-size: 12px;
 	border: 1px solid black;
 	height: 100vh;
   .block {
-
+    margin: auto;
+    border: 1px solid black;
+    height: 1rem;
+    width: 1rem;
   }
 }
 </style>
